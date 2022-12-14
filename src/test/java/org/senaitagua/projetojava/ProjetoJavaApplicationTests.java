0c480f520dae0840530decf117ed9e698aa9f2a0
ChristopherNascimento <christopher.nascimento12@gmail.com>
2022-12-13 21:16:47 -0300

    COMMIT ZERO

---
+++ src/test/java/org/senaitagua/projetojava/ProjetoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.senaitagua.projetojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
